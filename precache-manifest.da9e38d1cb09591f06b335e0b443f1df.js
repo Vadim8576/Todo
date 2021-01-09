dca30fd63c3667ef69cd5f30e910c036ae303df6
Vadim8576 <61579030+Vadim8576@users.noreply.github.com>
2021-01-09 17:09:33 +0300

    Updates

---
+++ precache-manifest.da9e38d1cb09591f06b335e0b443f1df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e883414354de677b2222530bc6c82af",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "0293f184ccb60c3d00a2",
+    "url": "/Todo/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "8a282301ea225d15270f",
+    "url": "/Todo/static/css/main.058494e3.chunk.css"
+  },
+  {
+    "revision": "0293f184ccb60c3d00a2",
+    "url": "/Todo/static/js/2.26fa3f0e.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/Todo/static/js/2.26fa3f0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a282301ea225d15270f",
+    "url": "/Todo/static/js/main.aece609f.chunk.js"
+  },
+  {
+    "revision": "01487ddd545745f7acc3",
+    "url": "/Todo/static/js/runtime-main.c45df924.js"
+  }
+]);
